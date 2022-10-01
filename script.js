95342193e5e6a1ee46390d4d05cc8e20595ce2c7
tanishq-soni <tanishqsoni49777@gmail.com>
2022-10-01 21:05:31 +0530

    v0.1

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function encrypt(){
     //document.getElementById("locked_url").value=EncryptED.toString();
     //console.log(EncryptED.toString());
     var url_hash = EncryptED.toString().replace(/\+/g,'p1L2u3S').replace(/\//g,'s1L2a3S4h').replace(/=/g,'e1Q2u3A4l');
-    var locked_url = "http://127.0.0.1:5500/?hash="+url_hash;
+    var locked_url = "https://url-locker.vercel.app/?hash="+url_hash;
     document.getElementById("lock_div_id").style.display="none";
     document.getElementById("url_display_id").style.display="block";
     document.getElementById("locked_url").innerHTML=locked_url;
